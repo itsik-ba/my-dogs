2d4cacf3ce0b3a539e4b86ba0b882b6fbe962587
StMauda <120678111+StMauda@users.noreply.github.com>
2024-01-16 22:30:13 +0200

    add picture and fix dogsdata

---
+++ dog-store/pages/DogsData.tsx
@@ -0,0 +1,32 @@
+export const DogsDataData = [
+  {
+    name: "Nils",
+    url: require("../assets/nils.jpg"),
+    price: "68$"
+  },
+  {
+    name: "Babsi",
+    url: require("../assets/babsi.jpg"),
+    price: "68$"
+  },
+  {
+    name: "NilsBabbls",
+    url: require("../assets/nilsbabbls.jpg"),
+    price: "68$"
+  },
+  {
+    name: "Babbls",
+    url: require("../assets/babbls.jpg"),
+    price: "68$"
+  },
+  {
+    name: "babblsi",
+    url: require("../assets/babblsi.jpg"),
+    price: "68$"
+  },
+  {
+    name: "Simon",
+    url: require("../assets/simon.jpg"),
+    price: "68$"
+  },
+];
